ea21f5aac0e833c188cdbdf680c3b662dfd2de2d
markus867-35 <markussuperhoki@gmail.com>
2026-09-24 03:05:18 +0700

    update

--- app/dashboard/add-data/file-image/page.tsx
+++ app/dashboard/add-data/file-image/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+export const dynamic = 'force-dynamic';
 import { useState, useEffect } from 'react';
 import { useTheme } from '@/app/context/ThemeContext';
 import { supabase } from '@/lib/supabase';
